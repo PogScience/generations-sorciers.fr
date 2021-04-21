2256ff1a8e0fbf3e474639ea76d0e1d807d97323
Amaury Carrade <amaury@carrade.eu>
2021-04-21 23:35:23 +0200

    Ajout d'un style pour le live final

--- htdocs/sections/events.php
+++ htdocs/sections/events.php
@@ -90,6 +90,44 @@ function caret() {
                     </div>
                 </details>
             </article>
+
+            <h2>Lundi 03 mars</h2>
+
+            <article class="box is-final">
+                <details>
+                    <summary>
+                        <time datetime="">
+                            20h
+                        </time>
+
+                        <div class="is-event-description">
+                            <h3><span class="live">LIVE</span> Sorciers Solidaires</h3>
+                            <p class="subtitle">La libre-antenne de clôture avec tout le monde !</p>
+                            <p class="streamers">
+                                Avec Jamy Gourmaud, Fred Courant, Sabine Quindou, Valérie Guerlain (la petite voix), …
+                            </p>
+                        </div>
+
+                        <aside>
+                            <time datetime="" class="is-mobile-time" aria-hidden="true">20h</time>
+
+                            <a href="#" class="button is-primary is-large" title="Aller sur la chaîne sur Twitch">
+                              <span class="icon is-large">
+                                <?php twitch(); ?>
+                              </span>
+                            </a>
+                            <span class="icon is-caret">
+                                <?php caret(); ?>
+                            </span>
+                        </aside>
+                    </summary>
+
+                    <div class="content">
+                        <p>Les livres ou textes feel good, avec commentaire de textes et/ou anecdotes.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda dignissimos eius enim eos exercitationem expedita facilis ipsum maiores minima mollitia pariatur perferendis quaerat, quos rerum sed sequi ullam voluptatibus.</p>
+                    </div>
+                </details>
+            </article>
         </div>
     </div>
 </section>
